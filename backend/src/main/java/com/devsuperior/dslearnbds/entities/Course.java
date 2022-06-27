ea0c99ba807bc62e7e20ef147d8310f4cc1fd3e8
Pedro Lima <pedrojppb@gmail.com>
2022-06-27 15:40:02 -0300

    offer

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearnbds.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +17,8 @@ public class Course implements Serializable {
     private String name;
     private String imgUri;
     private String imgGrayUri;
+    @OneToMany(mappedBy = "course")
+    private List<Offer> offers = new ArrayList<>();
 
     public Course() {
     }
@@ -54,8 +58,12 @@ public class Course implements Serializable {
         return imgGrayUri;
     }
 
-    public void setImgGrayUri(String imgGrayUrl) {
-        this.imgGrayUri = imgGrayUrl;
+    public void setImgGrayUri(String imgGrayUri) {
+        this.imgGrayUri = imgGrayUri;
+    }
+
+    public List<Offer> getOffers() {
+        return offers;
     }
 
     @Override
