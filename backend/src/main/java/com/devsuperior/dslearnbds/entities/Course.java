f8a3ba2cb61cea85ba7194f442e36adf741d4db7
Pedro Lima <pedrojppb@gmail.com>
2022-06-27 15:14:36 -0300

    Course

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -0,0 +1,73 @@
+package com.devsuperior.dslearnbds.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_course")
+public class Course implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String imgUri;
+    private String imgGrayUri;
+
+    public Course() {
+    }
+
+    public Course(Long id, String name, String imgUri, String imgGrayUrl) {
+        this.id = id;
+        this.name = name;
+        this.imgUri = imgUri;
+        this.imgGrayUri = imgGrayUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
+
+    public String getImgGrayUri() {
+        return imgGrayUri;
+    }
+
+    public void setImgGrayUri(String imgGrayUrl) {
+        this.imgGrayUri = imgGrayUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Course)) return false;
+        Course course = (Course) o;
+        return id.equals(course.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
